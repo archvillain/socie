694bbfb36a758700ce9f8aeae8e58c0da0527d07
johnjimysom <johnjimysom@yahoo.co.jp>
2018-10-10 02:44:05 -0700

    updated updates.py, revised

--- utils/updates.py
+++ utils/updates.py
@@ -3,17 +3,18 @@ def check_for_updates():
     from os import path
     
     if not path.exists(".git"):
-        print(gc.term.red("Error: client not started from repo location! Cancelling..."))
-        print(gc.term.yellow("You must start the client from its folder to get automatic updates. \n"))
+        print(gc.term.red("E̸̗͚̰̜͎̫̿̐̊̔͆̾̉̊̅̚͝RRƠ̸̳̙̙̯̌͒̓̽͒̒̄̓͒̽͒͊̅̍̐̇̋̽̅̎̿͒̈́̈͒̈́͝͝͠͝͝R: client not started from its directory! Cancelling..."))
+        print(gc.term.yellow("Please start the client from its folder to get automatic updates. \n自動アップデートを取得するには、そのフォルダからクライアントを起動する必要があります。"))
         return
 
     try:# git pull at start as to automatically update to master repo
         from subprocess import Popen,PIPE
-        print(gc.term.green + "Checking for updates..." + gc.term.normal)
+        print(gc.term.green + "ダウンロード中お待ちください。\n"+"\n"+"C̸̡̠̘̺͉̹̹͇͉̹̞̼̜̜͍̘̘̤̊́̍̒hecking fo̸̡̨͉͊̒͜r u̵̙͈̓̓́pdates for S̷̨̻͗̋͒ocieţ̵̛̩̰̬̒̓͝ţ̸̧̍̽̅̑̿e  🗲 「 ソシエット 」..." + gc.term.normal)
         process = Popen(["git", "pull", "--force"], stdout=PIPE)
         output = process.communicate()[0].decode('utf-8').strip()
 
-        if "Already up to date" not in output:
+        #if "Already up to date" not in output:
+        if "Already up to date. 更新は必要ありません。  S̷̨̻͗̋͒ocieţ̵̛̩̰̬̒̓͝ţ̸̧̍̽̅̑̿e  🗲 「 ソシエット 」は最新です。" not in output:
             # print(gc.term.yellow("Updates downloaded! Please restart."))
             print("\n \n")
             # This quit() call is breaking the client on MacOS and Linux Mint
@@ -21,13 +22,13 @@ def check_for_updates():
             # "Already up to date". Why is this happening?
             # quit()
         else:
-            print("Already up to date!" + "\n")
-    except KeyboardInterrupt: print("Call to cancel update received, skipping.")
+            print("It is on the latest build. No update needed \n更新は必要ありません。  S̷̨̻͗̋͒ocieţ̵̛̩̰̬̒̓͝ţ̸̧̍̽̅̑̿e  🗲 「 ソシエット 」は最新です。" + "\n")
+    except KeyboardInterrupt: print("Cancel update input received, skipping..." +"\n" +"更新がキャンセルされました。")
     except SystemExit: pass
     except OSError: # (file not found)
         # They must not have git installed, no automatic updates for them!
-        print(gc.term.red + "Error fetching automatic updates! Do you \
-              have git installed?" + gc.term.normal)
+        print(gc.term.red + "E̸̗͚̰̜͎̫̿̐̊̔͆̾̉̊̅̚͝RRƠ̸̳̙̙̯̌͒̓̽͒̒̄̓͒̽͒͊̅̍̐̇̋̽̅̎̿͒̈́̈͒̈́͝͝͠͝͝R: Could not get updates. Is \
+               git installed?" +"\n"+"ERROR: GITが必要です。" + gc.term.normal)
     except:
-        print(gc.term.red + "Unknown error occurred during retrieval \
-              of updates." + gc.term.normal)
+        print(gc.term.red + "Unknown E̸̗͚̰̜͎̫̿̐̊̔͆̾̉̊̅̚͝RRƠ̸̳̙̙̯̌͒̓̽͒̒̄̓͒̽͒͊̅̍̐̇̋̽̅̎̿͒̈́̈͒̈́͝͝͠͝͝R (error) occurred during \
+               updates." + gc.term.normal)
